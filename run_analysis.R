38aa8a441b2a0c8781ef2940a96e917290a145cc
senine <senine16@gmail.com>
2014-09-21 17:49:20 -0400

    Complete

--- run_analysis.R
+++ run_analysis.R
@@ -1,42 +1,35 @@
 ## requirement 1
 #training set
-subject<-read.table("~/train/subject_train.txt")
-X<- read.table("~/train/X_train.txt")
-y<- read.table("~/train/y_train.txt")
+subject<-read.table("./train/subject_train.txt")
+X<- read.table("./train/X_train.txt")
+y<- read.table("./train/y_train.txt")
 
 training <- cbind(subject,y,X)
 
 #test set
-subject1<-read.table("~/test/subject_test.txt")
-X1<- read.table("~/test/X_test.txt")
-y1<- read.table("~/test/y_test.txt")
+subject1<-read.table("./test/subject_test.txt")
+X1<- read.table("./test/X_test.txt")
+y1<- read.table("./test/y_test.txt")
 
 test <- cbind(subject1,y1,X1)
 
 # merge test and training
 data <- rbind(training, test)
 
-names <- as.character(read.table("~/features.txt")[,2])
+names <- as.character(read.table("./features.txt")[,2])
 
 ## requirement 4
-colnames(data) <- c("subject", "y", names)
+colnames(data) <- c("subject", "activity", names)
 
 ## requirement 2
-data1 <- data[,sort(c(1,grep("-mean\\(\\)-", names(data)),grep("-std\\(\\)-", names(data))))] 
+data1 <- data[,sort(c(1,2,grep("-mean\\(\\)-", names(data)),grep("-std\\(\\)-", names(data))))] 
 # checked, gravityMean is not a statistical method.
 
 
 
 ## requirement 5
-namelist <- t(as.data.frame(sapply(names(data1),strsplit,"-")))[,1]
-namelist <- as.character(namelist)[-1]
+newdata<- with(data1,split(data1, list(subject,activity)))
+newdata1 <-t(as.data.frame(lapply(newdata, colMeans)))
 
-data2 <- as.matrix(data1[,-1])
-colnames(data2) <- namelist
-rownames(data2) <- data[,1]
 
-library(reshape2)
-data3 <- melt(data2,varnames = c("subject", "activity"))
-newdata <- with(data3,tapply(value, list(subject,activity),mean))
-
-write.table(newdata, file = "prj.txt",row.name=FALSE)
+write.table(newdata1, file = "prj.txt",row.name=FALSE)
